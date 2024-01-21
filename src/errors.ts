837f9161a01f31f2bb26caa004306f2a507a1fc8
Alfredo Irarrazaval <airarrazaval@users.noreply.github.com>
2024-01-20 21:09:03 -0300

    added custom errors

---
+++ src/errors.ts
@@ -0,0 +1,36 @@
+import { ErrorResponse } from './response';
+
+export enum ErrorCodes {
+  InvalidCredentials = -5,
+  InvalidFrequency = -1,
+  InvalidSeries = -50,
+  InvalidDateRange = -1,
+  Unknown = -1,
+}
+
+export class WebServiceError extends Error {
+  response?: ErrorResponse;
+
+  constructor(response?: ErrorResponse, message?: string) {
+    super(message || response?.Descripcion || 'Unknown service error');
+    this.response = response;
+  }
+}
+
+export class InvalidFrequencyError extends WebServiceError {
+  constructor(response?: ErrorResponse) {
+    super(response, 'Invalid frequency code (must be DAILY, MONTHLY, QUARTERLY or ANNUAL)');
+  }
+}
+
+export class InvalidCredentialsError extends WebServiceError {
+  constructor(response?: ErrorResponse) {
+    super(response, 'Invalid username or password');
+  }
+}
+
+export class InvalidSeriesError extends WebServiceError {
+  constructor(response?: ErrorResponse) {
+    super(response, 'Invalid series id');
+  }
+}
