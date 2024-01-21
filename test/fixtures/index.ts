772ebfb2e518e9bbef9d7b9adeb701f5a0bf4afa
Alfredo Irarrazaval <airarrazaval@users.noreply.github.com>
2024-01-20 21:05:00 -0300

    added tests fixtures

---
+++ test/fixtures/index.ts
@@ -0,0 +1,15 @@
+import getSeriesSuccess from './responses/getseries.success.json';
+import getSeriesInvalid from './responses/getseries.invalid.json';
+import searchSeriesSuccess from './responses/searchseries.success.json';
+import searchSeriesInvalid from './responses/searchseries.invalid.json';
+import credentialsInvalid from './responses/credentials.invalid.json';
+
+export default {
+  response: {
+    credentialsInvalid,
+    getSeriesInvalid,
+    getSeriesSuccess,
+    searchSeriesInvalid,
+    searchSeriesSuccess,
+  },
+};
