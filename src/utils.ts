6effc2066ee63a408be78b977f50a35824d3aef6
Alfredo Irarrazaval <airarrazaval@users.noreply.github.com>
2025-02-18 19:57:59 -0300

    Refactor utility functions: isValidDate and reverseDate

--- src/utils.ts
+++ src/utils.ts
@@ -1,29 +1,24 @@
-import {
-  ErrorCodes,
-  InvalidFrequencyError,
-  InvalidCredentialsError,
-  InvalidSeriesError,
-  WebServiceError,
-} from './errors';
-import {
-  ApiResponse,
-  SeriesObservation,
-  SeriesMetadata,
-  ErrorResponse,
-  GetSeriesResponse,
-  SearchSeriesResponse,
-} from './types';
-
 /**
  * Determines wether a given value can be parsed to a valid date.
  */
 export function isValidDate(date: unknown): boolean {
   if (typeof date === 'string') {
-    return !Number.isNaN(Date.parse(date));
-  }
-  if (date instanceof Date) {
+    // Validate format YYYY-MM-DD
+    const isISOString = date.match(/^\d{4}-\d{2}-\d{2}$/);
+
+    if (!isISOString) {
+      return false;
+    }
+
+    // Validate parsed date timestamp
+    const timestamp = Date.parse(date);
+
+    return !Number.isNaN(timestamp);
+  } else if (date instanceof Date) {
+    // Validate date object
     return !Number.isNaN(date.getTime());
   }
+  // Is invalid date
   return false;
 }
 
@@ -31,58 +26,12 @@ export function isValidDate(date: unknown): boolean {
  * Reverses a date string from DD-MM-YYYY to YYYY-MM-DD format.
  */
 export function reverseDate(date: string): string {
+  if (/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+    return date;
+  }
+
+  if (!/^\d{2}-\d{2}-\d{4}$/.test(date)) {
+    throw new Error('Invalid date format. Expected DD-MM-YYYY');
+  }
   return date.split('-').reverse().join('-');
-}
-
-/**
- * Parses the GetSeries function API response.
- */
-export function parseGetSeriesResponse<T extends ApiResponse>(response: T): GetSeriesResponse {
-  if (response.Codigo !== 0) {
-    switch (response.Codigo) {
-      case ErrorCodes.InvalidCredentials:
-        throw new InvalidCredentialsError(response as ApiResponse as ErrorResponse);
-      case ErrorCodes.InvalidSeries:
-        throw new InvalidSeriesError(response as ApiResponse as ErrorResponse);
-      default:
-        throw new WebServiceError(response as ApiResponse as ErrorResponse);
-    }
-  }
-
-  return {
-    seriesId: response.Series.seriesId || '',
-    description: response.Series.descripIng || '',
-    data: (response.Series.Obs || []).map((obs: SeriesObservation) => ({
-      date: reverseDate(obs.indexDateString),
-      value: parseFloat(obs.value),
-    })),
-  };
-}
-
-/**
- * Parses the SearchSeries function API response.
- */
-export function parseSearchSeriesResponse<T extends ApiResponse>(
-  response: T,
-): SearchSeriesResponse {
-  if (response.Codigo !== 0) {
-    switch (response.Codigo) {
-      case ErrorCodes.InvalidCredentials:
-        throw new InvalidCredentialsError(response as ApiResponse as ErrorResponse);
-      case ErrorCodes.InvalidFrequency:
-        throw new InvalidFrequencyError(response as ApiResponse as ErrorResponse);
-      default:
-        throw new WebServiceError(response as ApiResponse as ErrorResponse);
-    }
-  }
-
-  return response.SeriesInfos.map((series: SeriesMetadata) => ({
-    seriesId: series.seriesId,
-    frequency: series.frequencyCode,
-    title: series.englishTitle,
-    firstObservedAt: reverseDate(series.firstObservation),
-    lastObservedAt: reverseDate(series.lastObservation),
-    updatedAt: reverseDate(series.updatedAt),
-    createdAt: reverseDate(series.createdAt),
-  }));
 }
